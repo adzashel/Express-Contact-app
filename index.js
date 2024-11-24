286b4c1e87a164a693e17f60b546603adf259c93
adzashel <seigneur.noir15@gmail.com>
2024-11-25 01:10:40 +0700

    validate name and email

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const chalk = require('chalk');
-const { renderContact,
-    detailContact,
-    addContact,
-    checkExistEmail,
-    checkDuplicate,
-    deleteContact,
-    duplicateEmail,
-    updateContact
-} = require('./utils/contacts');
+const chalk = require("chalk");
+const {
+  renderContact,
+  detailContact,
+  addContact,
+  checkExistEmail,
+  checkDuplicate,
+  deleteContact,
+  duplicateEmail,
+  updateContact,
+} = require("./utils/contacts");
 const port = 3000;
-const expressLayouts = require('express-ejs-layouts');
-const { validationResult, check, body } = require('express-validator');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const flash = require('connect-flash');
+const expressLayouts = require("express-ejs-layouts");
+const { validationResult, check, body } = require("express-validator");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const flash = require("connect-flash");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // built in middleware
-app.use(express.static('public')); // to show static files
+app.use(express.static("public")); // to show static files
 app.use(express.urlencoded({ extended: true }));
 // Third Party Middleware
 app.use(expressLayouts);
@@ -28,173 +29,185 @@ app.use(expressLayouts);
 // configure modules
 
 app.use(cookieParser());
-app.use(session({
-    secret: 'secret',
+app.use(
+  session({
+    secret: "secret",
     resave: true,
     saveUninitialized: true,
     cookie: {
-        maxAge: 600000, // 10 minutes
-    }
-}));
+      maxAge: 600000, // 10 minutes
+    },
+  })
+);
 
 app.use(flash());
 
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'home page',
-        layout: 'partials/container.ejs'
-    });
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "home page",
+    layout: "partials/container.ejs",
+  });
 });
 
-
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About',
-        layout: 'partials/container',
-    })
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+    layout: "partials/container",
+  });
 });
 
-
-app.get('/contact', (req, res) => {
-    const contacts = renderContact();
-    res.render('contact', {
-        title: 'Contact',
-        layout: 'partials/container',
-        contacts,
-        message: req.flash('message'),
-    });
+app.get("/contact", (req, res) => {
+  const contacts = renderContact();
+  res.render("contact", {
+    title: "Contact",
+    layout: "partials/container",
+    contacts,
+    message: req.flash("message"),
+  });
 });
 
-app.get('/contact/add', (req, res) => {
-    res.render('add-contact', {
-        title: 'Add Contact',
-        layout: 'partials/container',
-    });
+app.get("/contact/add", (req, res) => {
+  res.render("add-contact", {
+    title: "Add Contact",
+    layout: "partials/container",
+  });
 });
 
-app.post('/contact', [
-    body('name').custom((value) => {
-        const duplicate = checkDuplicate(value);
-        if (duplicate) {
-            throw new Error('Nama yang anda masukan sudah ada');
+app.post(
+  "/contact",
+  [
+    body("name")
+    .trim().notEmpty()
+    .withMessage("Please enter ur name")
+    .custom(async (value) => {
+        const isDuplicateName = checkDuplicate(value);
+        if(isDuplicateName) {
+            throw new Error("Name already exist !");
         }
         return true;
     }),
-    body('email').isEmail()
-        .withMessage('Format Email salah')
-        .custom((value) => {
-            const isDuplicateEmail = duplicateEmail(value);
-            if (isDuplicateEmail) {
-                throw new Error('Email sudah terdaftar');
-            } return true;
-        })
-],
-    (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            res.render('add-contact', {
-                title: 'Add Contact',
-                layout: 'partials/container',
-                errors: errors.array() // error kosong berisi undefined
-            })
-        } else {
-            addContact(req.body);
-            req.flash('message', 'Data has already been added')
-            console.log(chalk.bgBlue(`data ${req.body.name} added successfully`));
-            res.redirect('/contact');
+    body("email")
+      .isEmail()
+      .withMessage("Format Email salah")
+      .custom((value) => {
+        const isDuplicateEmail = duplicateEmail(value);
+        if (isDuplicateEmail) {
+          throw new Error("Email sudah terdaftar");
         }
-    });
-
+        return true;
+      }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("add-contact", {
+        title: "Add Contact",
+        layout: "partials/container",
+        errors: errors.array(), // error kosong berisi undefined
+      });
+    } else {
+      addContact(req.body);
+      req.flash("message", "Data has already been added");
+      console.log(chalk.bgBlue(`data ${req.body.name} added successfully`));
+      res.redirect("/contact");
+    }
+  }
+);
 
 //route delete contact
-app.get('/contact/delete-contact/:name', (req, res) => {
-    const contact = detailContact(req.params.name);
-    if (!contact) {
-        res.status(404).send("<h1>Contact not found</h1>");
-    } else {
-        // delete the contact
-        deleteContact(req.params.name);
-        req.flash('message', 'data has been deleted');
-        console.log(chalk.bgRed.inverse(`contact ${req.params.name} has been deleted`));
-        // redirect to cntacts page
-        res.redirect('/contact');
-    }
+app.get("/contact/delete-contact/:name", (req, res) => {
+  const contact = detailContact(req.params.name);
+  if (!contact) {
+    res.status(404).send("<h1>Contact not found</h1>");
+  } else {
+    // delete the contact
+    deleteContact(req.params.name);
+    req.flash("message", "data has been deleted");
+    console.log(
+      chalk.bgRed.inverse(`contact ${req.params.name} has been deleted`)
+    );
+    // redirect to cntacts page
+    res.redirect("/contact");
+  }
 });
 
 // route update contact
-app.get('/contact/update/:name', (req, res) => {
-    const contact = detailContact(req.params.name);
-    if (!contact) {
-        res.status(404).send("<h1>Contact not found</h1>");
-    } else {
-        res.render('update-contact', {
-            title: 'Update Contact',
-            layout: 'partials/container',
-            contact
-        });
-    }
+app.get("/contact/update/:name", (req, res) => {
+  const contact = detailContact(req.params.name);
+  if (!contact) {
+    res.status(404).send("<h1>Contact not found</h1>");
+  } else {
+    res.render("update-contact", {
+      title: "Update Contact",
+      layout: "partials/container",
+      contact,
+    });
+  }
 });
 
 // processing form update contact
-app.post('/contact/update', [
-    body('name').custom((value, { req }) => {
-        const duplicate = checkDuplicate(value);
-        if (value !== req.body.prevName && duplicate) {
-            throw new Error('Nama yang anda masukan sudah ada');
-        }
-        return true;
+app.post(
+  "/contact/update",
+  [
+    body("name").custom((value, { req }) => {
+      const duplicate = checkDuplicate(value);
+      if (value !== req.body.prevName && duplicate) {
+        throw new Error("Nama yang anda masukan sudah ada");
+      }
+      return true;
     }),
-    body('email')
-        .isEmail()
-        .withMessage('Format Email salah')],
-    (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            res.render('update-contact', {
-                title: 'Update Contact',
-                layout: 'partials/container',
-                contact: req.body,
-                errors: errors.array() // error kosong berisi undefined
-            })
-        } else {
-            updateContact(req.body);
-            req.flash('message', 'Data has been updated');
-            console.log(chalk.bgYellow.inverse(`contact ${req.body.prevName} has been updated`));
-            res.redirect('/contact');
-        }
-    })
-app.get('/contact/:name', (req, res) => {
-    const detail = detailContact(req.params.name);
-    res.render('detail', {
-        title: 'Detail Contact',
-        layout: 'partials/container',
-        detail
-    });
+    body("email").isEmail().withMessage("Format Email salah"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("update-contact", {
+        title: "Update Contact",
+        layout: "partials/container",
+        contact: req.body,
+        errors: errors.array(), // error kosong berisi undefined
+      });
+    } else {
+      updateContact(req.body);
+      req.flash("message", "Data has been updated");
+      console.log(
+        chalk.bgYellow.inverse(`contact ${req.body.name} has been updated`)
+      );
+      res.redirect("/contact");
+    }
+  }
+);
+app.get("/contact/:name", (req, res) => {
+  const detail = detailContact(req.params.name);
+  res.render("detail", {
+    title: "Detail Contact",
+    layout: "partials/container",
+    detail,
+  });
 });
 
-
-app.get('/api/', (req, res) => {
-    const data = [
-        {
-            name: "Freidrich Nietzsche",
-            quote: "In Heaven , all interesting people are missing"
-        },
-        {
-            name: "Socrates",
-            quote: "Everything has a beginning, but not everything has a destination"
-        }
-    ]
-    res.json(data);
+app.get("/api/", (req, res) => {
+  const data = [
+    {
+      name: "Freidrich Nietzsche",
+      quote: "In Heaven , all interesting people are missing",
+    },
+    {
+      name: "Socrates",
+      quote: "Everything has a beginning, but not everything has a destination",
+    },
+  ];
+  res.json(data);
 });
 
 app.use((req, res) => {
-    res.status(404);
-    res.render('404', {
-        layout: 'partials/failed',
-        title: 'Page not found',
-    })
-})
+  res.status(404);
+  res.render("404", {
+    layout: "partials/failed",
+    title: "Page not found",
+  });
+});
 
 app.listen(port, () => {
-    console.log(chalk.bgGreen(`Server is running at http://localhost:${port}`));
+  console.log(chalk.bgGreen(`Server is running at http://localhost:${port}`));
 });
